3839a82471cec9986947147feab88d8f7bc98769
sivaprasadtalapagala <sivaprasadreddytalapagala@gmail.com>
2024-01-20 23:45:31 +0530

    basic tasks done

---
+++ basic.js
@@ -0,0 +1,48 @@
+//1.Access an Array Element:
+// Question: "Given the array let myArray = [10, 20, 30, 40, 50];, what is the value at index 2?"
+// Output: 30
+
+// let myArray = [10, 20, 30, 40, 50];
+// console.log("index at 2 is",myArray[2]);
+
+// ==============================
+
+// 2.Modify an Array Element:
+
+// Question: "If myArray is [10, 20, 30, 40, 50], what is the array after changing the value at index 1 to 25?"
+// Output: [10, 25, 30, 40, 50]
+
+// let myArray = [10, 20, 30, 40, 50];
+// myArray[1]=25;
+// console.log("2 ans",myArray)
+
+// ==============================
+
+// 3.String Character Access:
+
+// Question: "For the string let myString = 'Hello';, what is the character at index 4?"
+// Output: o
+
+// let myString = 'Hello';
+// console.log("3rd ans",myString[4])
+
+// ==============================
+
+// 4.Update String Character:
+
+// Question: "If myString is 'Hello', what is the string after changing the character at index 2 to 'x'?"
+// Output: Helxo
+
+// let myString = 'Hello';
+// myString[2]="x";
+// console.log('4th ans',myString);//4th ans Hello
+
+// let convertedArray=myString.split("");
+// convertedArray[2]="x";
+// console.log("4th converted ans",convertedArray)//4th converted ans [ 'H', 'e', 'x', 'l', 'o' ]
+// myString=convertedArray.join('');
+// console.log('4th ans after joing',myString);//4th ans Hello
+
+// ==============================
+
+
